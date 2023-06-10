7ecc133e6cde4b7605a16890d3aa4d9ca4a0a813
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-06-10 11:15:33 -0300

    update

--- Simulador.entrega.encomendas.py
+++ Simulador.entrega.encomendas.py
@@ -1,4 +1,4 @@
-km = int(input("Digite adistância  em  KM :"))
+km = int(input("Digite a distância da entrega em  KM :"))
 print("O meio transporte a ser utilizado será o :")
 print("terrestre" if km <= 500 else  "Aéreo")
 print("Obrigado por usar nossos seriços!")
