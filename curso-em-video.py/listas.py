31978f049e1758a95ad59feda17a7aae70db23ef
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-06-24 19:51:28 -0300

    new update

---
+++ curso-em-video.py/listas.py
@@ -0,0 +1,11 @@
+lista= []
+lista.append(2)
+lista.append(6)
+lista.append(1)
+lista.append(3)
+lista.append(5)
+lista.append(4)
+for c, v in enumerate (lista):
+    print(f"Na posição {c} o número é {v}")
+print("progama encerrado")
+#progama em andamento
