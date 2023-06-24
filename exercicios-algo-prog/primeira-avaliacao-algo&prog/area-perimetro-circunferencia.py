4b5e9d73e572d4384f6c85fd3342717341ac0411
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-06-23 21:31:46 -0300

    update-23-jun

--- exercicios-algo-prog/primeira-avaliacao-algo&prog/area-perimetro-circunferencia.py
+++ exercicios-algo-prog/primeira-avaliacao-algo&prog/area-perimetro-circunferencia.py
@@ -3,4 +3,4 @@ raio= float(input("Digite o valor do raio da circunferência: "))
 area= m.pi * m.pow(raio, 2)
 peri= m.tau * raio
 print("A área do circulo é: {:.2f}\nE seu perimetro é: {:.2f}".format(area, peri))
-print("progama encerrado!")
+print("progama encerrado!")  
