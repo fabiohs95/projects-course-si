ddd922a9d2f0a1f3efcce6e37a27f499d662677c
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-06-26 20:48:45 -0300

    update-25-jun

--- curso-em-video.py/listas.py
+++ curso-em-video.py/listas.py
@@ -1,11 +1,8 @@
-lista= []
-lista.append(2)
-lista.append(6)
-lista.append(1)
-lista.append(3)
-lista.append(5)
-lista.append(4)
-for c, v in enumerate (lista):
-    print(f"Na posição {c} o número é {v}")
-print("progama encerrado")
-#progama em andamento
+r= "S"
+while not r:
+    e= int(input("digite um número; "))
+    r= str(input("Deseja continuar? [S/N]")).upper()
+    if "S" < 2:
+        print("O seu número da sorte é: {}".format(e))
+    elif "S" > 2:
+        print("Seu número da sote é a soma entre esses números {}".format(e))
