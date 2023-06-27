9d412f7b07785498c1c7dbcc1df5880f696f7f25
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-06-26 21:50:48 -0300

    redução do código/adiçao do "for"

--- exercicios-algo-prog/primeira-avaliacao-algo&prog/numero-maior.py
+++ exercicios-algo-prog/primeira-avaliacao-algo&prog/numero-maior.py
@@ -1,6 +1,4 @@
-n1= float(input("digite o primeiro número: "))
-n2= float(input("digite o segundo número: "))
-n3= float(input("digite mais um número: "))
 lista= []
-lista.extend([n1, n2, n3])
+for cont in range(0, 3):
+    lista.append(float(input("digite um valor: ")))
 print(f"dentre os números {lista} \n O maior é:>>{round(max(lista), 2)}<<")
