d2dc130539a0ffaae61dd114a3f5a408731e0aa9
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-07-08 22:05:00 -0300

    criação de um novo algoritmo

---
+++ curso-em-video.py/contagem.py
@@ -0,0 +1,6 @@
+mochila= ["roupas","livros","carteira","toalha","caixiha de som"]
+for item in mochila:
+    print(item, "e" ,mochila[-1])
+    print(type(mochila))
+else:
+    print("Todos os itens foram listados!")
