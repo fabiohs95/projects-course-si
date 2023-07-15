70a65b536381d0a7f6c8480b7dac987be1460f68
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-07-15 11:42:32 -0300

    update/alteração nos nomes das variáveis/mais dica

--- exercicios-algo-prog/atividades-julho/NumeroAleatorio.py
+++ exercicios-algo-prog/atividades-julho/NumeroAleatorio.py
@@ -1,14 +1,22 @@
 import random
 menu= int(input("Escolha uma opção para definir o nível de dificuldade!\nOpção 1 (Fácil)\nOpção 2 (Médio)\nOpção 3 (Difícil)\nOpção 4 (Insano)\nDigite a opção:"))
 if menu == 1:
-    o1= int(input("Digite um número de 1 a 10:-> "))
-    sort= random.randint(1, 10)
+    n= int(input("Digite um número de 1 a 10:-> "))
+    sorte= random.randint(1, 10)
     cont= 1
-    while o1 != sort:
+    while n != sorte:
         cont= cont + 1
-        if o1 < sort:
-            o1= int(input("Ainda não é esse!\nUm pouco mais pra  cima!\nDigite: "))
-        elif o1 > sort:
-            o1= int(input("Ainda não é esse!\nTente um pouco mais pra baixo!\nDigite: "))
+        if n < sorte and sorte - n <= 2:
+            n= int(input("Ainda não é esse!\nUm pouco mais pra  cima!\nDigite: "))
+        elif n < sorte and sorte - n >= 3 and sorte - n < 6:
+            n= int(input("Ainda não é esse!\nTente mais pra cima!\nDigite: "))
+        elif n < sorte and sorte - n >= 6:
+            n= int(input("Ainda não é esse!\nBem mais pra cima!\nDigite: "))
+        elif n > sorte and n - sorte <=2:
+            n= int(input("Ainda não é esse!\nTente um pouco mais pra baixo!\nDigite: "))
+        elif n > sorte and n - sorte >= 3 and n - sorte < 6:
+            n= int(input("Ainda não é esse!\nTente mais pra baixo!\nDigite: "))
+        elif n > sorte and n - sorte >= 6:
+            n= int(input("Ainda não é esse!\nBem mais pra baixo!\nDigite: "))
     else:
-        print("Parabéns! Você acertou na {}° tentativa! O número sorteado foi: {}".format(cont, sort))
+        print("Parabéns! Você acertou na {}° tentativa! O número sorteado foi: {}".format(cont, sorte))
