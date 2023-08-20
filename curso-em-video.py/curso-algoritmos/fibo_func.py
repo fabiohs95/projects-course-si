4192f323053d1f037ab18853b5c29736d1061bdc
Fábio Henrique dos Santos Gomes <fabio.gomes@academico.uncisal.edu.br>
2023-08-20 17:28:05 -0300

    Update/adição do except e de cores

--- curso-em-video.py/curso-algoritmos/fibo_func.py
+++ curso-em-video.py/curso-algoritmos/fibo_func.py
@@ -9,17 +9,20 @@ def fibo (a):
         print(n1)
 
 print('='*50)
-n= int(input('Digite um número: '))
-print(f'A sequência de fibonacci com {n} números é:')
-f= fibo(n)
-print('='*50)
-flag= str(input('Quer entrar com um novo valor? (S/N): ')).strip().lower()
-while flag == 's':
-    n=int(input('Insira um novo valor: '))
+try:
+    n= int(input('\033[1;30;42mDigite um número:\033[m '))
+    print(f'\033[1;32m sequência de fibonacci com {n} números é:\033[m')
     f= fibo(n)
-    flag= str(input('Quer entrar com um novo valor? (S/N): ')).strip().lower()
-    breakpoint
     print('='*50)
-if flag == 'n':
-    print('Tá bom, então!\nAté mais!')
-    print('='*50)
+    flag= str(input('\033[1;34mQuer entrar com um novo valor? (S/N):\033[m ')).strip().lower()
+    while flag == 's':
+        n=int(input('\033[1;32mnsira um novo valor:\033[m '))
+        f= fibo(n)
+        flag= str(input('\033[1;34mQuer entrar com um novo valor? (S/N):\033[m ')).strip().lower()
+        breakpoint
+        print('='*50)
+    if flag == 'n':
+        print('\033[1;33mTá bom, então!\nAté mais\033[m')
+        print('='*50)
+except Exception as error:
+    print('\033[1;41mEntrada inválida\033[m' )
